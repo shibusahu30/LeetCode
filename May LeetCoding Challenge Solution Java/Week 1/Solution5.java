f53d34d53da5f3878f415d226e22f46ab05735fc
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-05-05 14:04:23 +0530

    Update Solution5.java

--- May LeetCoding Challenge Solution Java/Week 1/Solution5.java
+++ May LeetCoding Challenge Solution Java/Week 1/Solution5.java
@@ -1,1 +1,29 @@
-// Error 404
+/*
+  First Unique Character in a String
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+
+Note: You may assume the string contain only lowercase letters. 
+*/
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0)+1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (map.get(ch) == 1) return i;
+        }
+        return -1;
+    }
+}
