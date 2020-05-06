88050b0d3f5c41623cdf716cd88539ba4d579607
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-05-06 13:43:56 +0530

    Update Main.java

--- JobPreparation/Amazon/Main.java
+++ JobPreparation/Amazon/Main.java
@@ -1,126 +1,24 @@
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.math.*; 
-public class Main1 {
-
-    public static void main(String[] args) throws Exception{
-		
-		LinkedList list = new LinkedList<>();
-		list.add(5);
-		list.add("3");
-		list.add(10);
-		System.out.println(list.get(1) instanceof Object);
-	}
-	
-}
-
-
-
-class Scan
-{
-    private byte[] buf=new byte[1024];
-    private int index;
-    private InputStream in;
-    private int total;
-    public Scan()
-    {
-        in=System.in;
-    }
-    public int scan()throws IOException
-    {
-        if(total<0)
-        throw new InputMismatchException();
-        if(index>=total)
-        {
-            index=0;
-            total=in.read(buf);
-            if(total<=0)
-            return -1;
-        }
-        return buf[index++];
-    }
-    public int scanInt()throws IOException
-    {
-        int integer=0;
-        int n=scan();
-        while(isWhiteSpace(n))
-        n=scan();
-        int neg=1;
-        if(n=='-')
-        {
-            neg=-1;
-            n=scan();
-        }
-        while(!isWhiteSpace(n))
-        {
-            if(n>='0'&&n<='9')
-            {
-                integer*=10;
-                integer+=n-'0';
-                n=scan();
-            }
-            else throw new InputMismatchException();
-        }
-        return neg*integer;
-    }
-    public double scanDouble()throws IOException
-    {
-        double doub=0;
-        int n=scan();
-        while(isWhiteSpace(n))
-        n=scan();
-        int neg=1;
-        if(n=='-')
-        {
-            neg=-1;
-            n=scan();
-        }
-        while(!isWhiteSpace(n)&&n!='.')
-        {
-            if(n>='0'&&n<='9')
-            {
-                doub*=10;
-                doub+=n-'0';
-                n=scan();
-            }
-            else throw new InputMismatchException();
-        }
-        if(n=='.')
-        {
-            n=scan();
-            double temp=1;
-            while(!isWhiteSpace(n))
-            {
-                if(n>='0'&&n<='9')
-                {
-                    temp/=10;
-                    doub+=(n-'0')*temp;
-                    n=scan();
+// https://leetcode.com/problems/two-sum/
+class Solution {
+    public long reducingDishes(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        int sum = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < 0) {
+                if (Math.abs(nums[i]) > sum) continue;
+                else {
+                    sum += nums[i];
+                    list.add(nums[i]);
                 }
-                else throw new InputMismatchException();
+            }else {
+                sum += nums[i];
+                list.add(nums[i]);
             }
         }
-        return doub*neg;
-    }
-    public String scanString()throws IOException
-    {
-        StringBuilder sb=new StringBuilder();
-        int n=scan();
-        while(isWhiteSpace(n))
-        n=scan();
-        while(!isWhiteSpace(n))
-        {
-            sb.append((char)n);
-            n=scan();
+        long answer = 0;
+        for (int i = list.size() - 1; i >= 0; i--) {
+            answer += (i+1) * (list.get(i));
         }
-        return sb.toString();
-    }
-    private boolean isWhiteSpace(int n)
-    {
-        if(n==' '||n=='\n'||n=='\r'||n=='\t'||n==-1)
-        return true;
-        return false;
+        return answer;
     }
 }
