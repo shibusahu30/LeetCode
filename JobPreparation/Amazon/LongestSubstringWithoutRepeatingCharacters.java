d228cef7ebd365b9606b6d749aaa4a6bc2161302
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-05-09 14:21:10 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- JobPreparation/Amazon/LongestSubstringWithoutRepeatingCharacters.java
+++ JobPreparation/Amazon/LongestSubstringWithoutRepeatingCharacters.java
@@ -28,6 +28,12 @@ class Solution {
         for(int i = 0,j = 0; j < s.length();j++) {
             if(map.containsKey(s.charAt(j))) {
                 // left starting range is the max of last occurence of right range character and the current left range
+                // lets say string a b c c f g h d c
+                //                 | | | | | | | | |
+                //                 0 1 2 3 4 5 6 7 8
+                //                         |       |
+                //                         i       j
+                // map will return map.get('c') i.e 3 but i is at 4, so choose 4 as next i
                 i = Math.max(i,map.get(s.charAt(j)));
             }
             max = Math.max(max,j-i+1);
