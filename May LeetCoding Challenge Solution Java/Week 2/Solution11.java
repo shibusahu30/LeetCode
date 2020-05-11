6df3a0a9c40cbe689c0c714a8747730a98b45ed8
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-05-11 15:55:17 +0530

    Update Solution11.java

--- May LeetCoding Challenge Solution Java/Week 2/Solution11.java
+++ May LeetCoding Challenge Solution Java/Week 2/Solution11.java
@@ -1,1 +1,51 @@
-// Error 404
+/*
+Flood Fill
+
+An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+At the end, return the modified image.
+
+Example 1:
+
+Input: 
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: 
+From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+by a path of the same color as the starting pixel are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected
+to the starting pixel.
+
+Note:
+The length of image and image[0] will be in the range [1, 50].
+The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+   Hide Hint #1  
+Write a recursive function that paints the pixel if it's the correct color, then recurses on neighboring pixels.
+*/
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (newColor == image[sr][sc]) return image;
+        foo(image, sr, sc, newColor, image[sr][sc]);
+        return image; 
+    }
+    void foo(int[][] im, int sr, int sc, int nc, int cc) {
+        // error case
+        if(sr < 0 || sc < 0 || sr >= im.length || sc >= im[sr].length) return;
+        // base case
+        if (im[sr][sc] != cc) return;
+        // actions
+        im[sr][sc] = nc;
+        // next call
+        foo(im, sr-1, sc, nc, cc);
+        foo(im, sr+1, sc, nc, cc);
+        foo(im, sr, sc+1, nc, cc);
+        foo(im, sr, sc-1, nc, cc);
+    }
+}
