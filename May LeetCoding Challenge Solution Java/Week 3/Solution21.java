dacd465b7722eed44b7deda99de602b5bf03b390
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-05-21 14:56:22 +0530

    Update Solution21.java

--- May LeetCoding Challenge Solution Java/Week 3/Solution21.java
+++ May LeetCoding Challenge Solution Java/Week 3/Solution21.java
@@ -1,1 +1,64 @@
-// Error 404
+/*
+Count Square Submatrices with All Ones
+
+Given a m * n matrix of ones and zeros, return how many square submatrices have all ones.
+
+ 
+
+Example 1:
+
+Input: matrix =
+[
+  [0,1,1,1],
+  [1,1,1,1],
+  [0,1,1,1]
+]
+Output: 15
+Explanation: 
+There are 10 squares of side 1.
+There are 4 squares of side 2.
+There is  1 square of side 3.
+Total number of squares = 10 + 4 + 1 = 15.
+
+Example 2:
+
+Input: matrix = 
+[
+  [1,0,1],
+  [1,1,0],
+  [1,1,0]
+]
+Output: 7
+Explanation: 
+There are 6 squares of side 1.  
+There is 1 square of side 2. 
+Total number of squares = 6 + 1 = 7.
+
+ 
+
+Constraints:
+
+    1 <= arr.length <= 300
+    1 <= arr[0].length <= 300
+    0 <= arr[i][j] <= 1
+
+   Hide Hint #1  
+Create an additive table that counts the sum of elements of submatrix with the superior corner at (0,0).
+   Hide Hint #2  
+Loop over all subsquares in O(n^3) and check if the sum make the whole array to be ones, if it checks then add 1 to the answer.
+*/
+class Solution {
+    public int countSquares(int[][] m) {
+        if (m.length == 0) return 0;
+        int[][] dp = new int[m.length][m[0].length];
+        int ans = 0;
+        for (int i= 0; i < m.length; ++i) {
+            for (int j = 0; j < m[i].length; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = m[i][j];
+                else dp[i][j] = m[i][j] == 1 ? Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1]))+1 : 0;
+                ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+}
