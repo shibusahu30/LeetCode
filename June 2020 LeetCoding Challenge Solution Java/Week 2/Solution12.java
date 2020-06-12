1a20b0a7ef3123db2f4b52575a3f96ff69bfca83
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-06-12 13:11:11 +0530

    day 12 solution added.

--- June 2020 LeetCoding Challenge Solution Java/Week 2/Solution12.java
+++ June 2020 LeetCoding Challenge Solution Java/Week 2/Solution12.java
@@ -1,1 +1,75 @@
-// Error 404
+/*
+Insert Delete GetRandom O(1)
+
+Design a data structure that supports all following operations in average O(1) time.
+
+    insert(val): Inserts an item val to the set if not already present.
+    remove(val): Removes an item val from the set if present.
+    getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+
+Example:
+
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+*/
+class RandomizedSet {
+    private Map<Integer, Integer> numIndexMap;
+    private List<Integer> numList;
+
+    public RandomizedSet() {
+        numIndexMap = new HashMap<>();
+        numList = new ArrayList<>();
+    }
+
+    public boolean insert(int val) {
+        if (numIndexMap.get(val) == null) {
+            numList.add(val);
+            numIndexMap.put(val, numList.size() - 1);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public boolean remove(int val) {
+        Integer valIdx = numIndexMap.get(val);
+        
+        if (valIdx != null) {
+            int size = numList.size();
+            int lastVal = numList.get(size - 1);
+            numList.set(valIdx, lastVal);
+            numIndexMap.put(lastVal, valIdx);
+            numList.remove(size - 1);
+            numIndexMap.remove(val);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public int getRandom() {
+        int randomIndex = new Random().nextInt(numList.size());
+        return numList.get(randomIndex);
+    }
+}
