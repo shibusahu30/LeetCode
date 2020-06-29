7263800c4dd2769fe73d13d8915a0bd8272407e3
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-06-29 20:04:23 +0530

    Day 29 solution added.

--- June 2020 LeetCoding Challenge Solution Java/Week 5/Solution29.java
+++ June 2020 LeetCoding Challenge Solution Java/Week 5/Solution29.java
@@ -1,1 +1,44 @@
-// Error 404
+/*
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+ _ _ _ _ _ _ _ 
+|S|_|_|_|_|_|_|
+|_|_|_|_|_|_|_|
+|_|_|_|_|_|_|F|
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+
+Constraints:
+
+    1 <= m, n <= 100
+    It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
+*/
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = 1;
+                else {
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
