9b36895dbb28c2f99052c55c76de81f48363e904
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-07-08 13:38:36 +0530

    BS Day 8 solution added.

--- July 2020 LeetCoding Challenge Solution Java/Week 2/Solution8.java
+++ July 2020 LeetCoding Challenge Solution Java/Week 2/Solution8.java
@@ -1,1 +1,75 @@
-// Error 404
+/*
+3Sum
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note:
+
+The solution set must not contain duplicate triplets.
+
+Example:
+
+Given array nums = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+
+   Hide Hint #1  
+So, we essentially need to find three numbers x, y, and z such that they add up to the given value. If we fix one of the numbers say x, we are left with the two-sum problem at hand!
+   Hide Hint #2  
+For the two-sum problem, if we fix one of the numbers, say
+
+x
+
+, we have to scan the entire array to find the next number
+
+y
+
+which is
+
+value - x
+
+where value is the input parameter. Can we change our array somehow so that this search becomes faster?
+   Hide Hint #3  
+The second train of thought for two-sum is, without changing the array, can we use additional space somehow? Like maybe a hash map to speed up the search?
+*/
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> sol = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return sol;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+			if (nums[i] > 0) break; 
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int base = i;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[base] + nums[left] + nums[right];
+                if (sum == 0) {
+                    sol.add(Arrays.asList(nums[base], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return sol;
+    }
+}
