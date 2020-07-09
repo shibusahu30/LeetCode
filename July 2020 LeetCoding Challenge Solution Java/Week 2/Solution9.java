39c8e61502e65bc2e931a9e7dd6f939f5a87728a
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-07-09 16:46:41 +0530

    Day 9 solution added.

--- July 2020 LeetCoding Challenge Solution Java/Week 2/Solution9.java
+++ July 2020 LeetCoding Challenge Solution Java/Week 2/Solution9.java
@@ -1,1 +1,99 @@
-// Error 404
+/*
+Maximum Width of Binary Tree
+
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        
+        int maxWidth = 0;
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+        while(!queue.isEmpty()){
+            Pair<TreeNode, Integer> first = queue.peek();
+            int size = queue.size();
+            Pair<TreeNode, Integer> curr = null;
+            while(size-- > 0){
+                curr = queue.poll();
+                TreeNode node = curr.getKey();
+                int index = curr.getValue();
+                if(node.left != null) queue.offer(new Pair(node.left, 2 * index));
+                if(node.right != null) queue.offer(new Pair(node.right, 2 * index + 1));
+            }
+            maxWidth = Math.max(maxWidth, curr.getValue() - first.getValue() + 1);
+        }
+        
+        return maxWidth;
+    }
+}
