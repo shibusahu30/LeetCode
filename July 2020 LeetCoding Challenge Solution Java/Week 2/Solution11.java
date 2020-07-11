964b89758b725ec65c5faeae20bf07c3040d8f3b
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-07-11 15:18:58 +0530

    Day 11 solution added.

--- July 2020 LeetCoding Challenge Solution Java/Week 2/Solution11.java
+++ July 2020 LeetCoding Challenge Solution Java/Week 2/Solution11.java
@@ -1,1 +1,42 @@
-// Error 404
+/*
+Subsets
+
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+class Solution {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> output = new ArrayList();
+    int n = nums.length;
+
+    for (int i = (int)Math.pow(2, n); i < (int)Math.pow(2, n + 1); ++i) {
+      // generate bitmask, from 0..00 to 1..11
+      String bitmask = Integer.toBinaryString(i).substring(1);
+        //System.out.println(bitmask);
+
+      // append subset corresponding to that bitmask
+      List<Integer> curr = new ArrayList();
+      for (int j = 0; j < n; ++j) {
+        if (bitmask.charAt(j) == '1') curr.add(nums[j]);
+      }
+      output.add(curr);
+    }
+    return output;
+  }
+}
