a9933f3f68f72e8dca37f82a46fe166e92023a18
shibashis sahu <44725484+shibusahu30@users.noreply.github.com>
2020-07-14 14:42:28 +0530

    Day 14 solution added.

--- July 2020 LeetCoding Challenge Solution Java/Week 2/Solution14.java
+++ July 2020 LeetCoding Challenge Solution Java/Week 2/Solution14.java
@@ -1,1 +1,55 @@
-// Error 404
+/*
+Angle Between Hands of a Clock
+
+Given two numbers, hour and minutes. Return the smaller angle (in degrees) formed between the hour and the minute hand.
+
+ 
+
+Example 1:
+
+Input: hour = 12, minutes = 30
+Output: 165
+
+Example 2:
+
+Input: hour = 3, minutes = 30
+Output: 75
+
+Example 3:
+
+Input: hour = 3, minutes = 15
+Output: 7.5
+
+Example 4:
+
+Input: hour = 4, minutes = 50
+Output: 155
+
+Example 5:
+
+Input: hour = 12, minutes = 0
+Output: 0
+
+ 
+
+Constraints:
+
+    1 <= hour <= 12
+    0 <= minutes <= 59
+    Answers within 10^-5 of the actual value will be accepted as correct.
+
+   Hide Hint #1  
+The tricky part is determining how the minute hand affects the position of the hour hand.
+   Hide Hint #2  
+Calculate the angles separately then find the difference.
+*/
+class Solution {
+    public double angleClock(int h, int m) {
+        double ans = 0.0, mA, hA;
+        if(h == 12) h = 0;
+        mA = 6 * m;
+        hA = 0.5 * (h*60 + m);
+        ans = Math.abs(mA - hA);
+        return Math.min(ans, 360-ans);
+    }
+}
