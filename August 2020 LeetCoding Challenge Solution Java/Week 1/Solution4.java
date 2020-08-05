7d711ac4bca891fb05a43280daeb9b615f26c782
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-08-05 21:09:18 +0530

    Day 4 solution added.

--- August 2020 LeetCoding Challenge Solution Java/Week 1/Solution4.java
+++ August 2020 LeetCoding Challenge Solution Java/Week 1/Solution4.java
@@ -1,1 +1,25 @@
-// Error 404
+/*
+Power of Four
+
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+
+Example 2:
+
+Input: 5
+Output: false
+
+Follow up: Could you solve it without loops/recursion?
+*/
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        String s = Integer.toBinaryString(num);
+        // if bin representation consists of 1 folllowed by even no of 0's
+        if((s.length()&1) == 1 && Integer.bitCount(num) == 1) return true;
+        return false;
+    }
+}
