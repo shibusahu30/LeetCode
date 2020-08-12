6ec4a5a4b38d918964c5a5af2326a78aa1984cae
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-08-12 19:34:57 +0530

    Day 12 solution added.

--- August 2020 LeetCoding Challenge Solution Java/Week 2/Solution12.java
+++ August 2020 LeetCoding Challenge Solution Java/Week 2/Solution12.java
@@ -1,1 +1,34 @@
-// Error 404
+/*
+Pascal's Triangle II
+
+Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+*/
+class Solution {
+    public List<Integer> getRow(int r) {
+        
+        int[] a = new int[r+1];
+        a[0] = 1;
+        for(int i=1; i<r+1; i++)
+            for(int j=i; j>=1; j--)
+                a[j] += a[j-1];
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0; i<r+1; i++) {
+            ans.add(a[i]);
+        }
+        return ans;
+    }
+}
