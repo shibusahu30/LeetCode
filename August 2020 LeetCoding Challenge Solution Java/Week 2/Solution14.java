07131f10718a93797bf4cdf20f3ffcc8bdf7a62e
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-08-14 13:51:36 +0530

    Day 14 solution added.

--- August 2020 LeetCoding Challenge Solution Java/Week 2/Solution14.java
+++ August 2020 LeetCoding Challenge Solution Java/Week 2/Solution14.java
@@ -1,1 +1,43 @@
-// Error 404
+/*
+Longest Palindrome
+
+Given a string which consists of lowercase or uppercase letters, find the length of the longest palindromes that can be built with those letters.
+
+This is case sensitive, for example "Aa" is not considered a palindrome here.
+
+Note:
+Assume the length of given string will not exceed 1,010.
+
+Example:
+
+Input:
+"abccccdd"
+
+Output:
+7
+
+Explanation:
+One longest palindrome that can be built is "dccaccd", whose length is 7.
+*/
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+        int ans = 0;
+        Map<Character, Integer> m = new HashMap<>();
+        for(char ch: s.toCharArray()) {
+            m.put(ch, m.getOrDefault(ch, 0)+1);
+        }
+        boolean isOdd = false;
+        for(Map.Entry<Character, Integer> it : m.entrySet()) {
+            int v = it.getValue();
+            if((v&1) == 0) ans += v;
+            else {
+                ans += v-1;
+                isOdd = true;
+            }
+        }
+        if(isOdd) ans += 1;
+        return ans;
+    }
+}
