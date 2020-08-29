cb7f30dedf9996dce89e2bb82e4f14f936438d58
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-08-29 07:44:10 +0530

    Day 28 solution added.

--- August 2020 LeetCoding Challenge Solution Java/Week 4/Solution28.java
+++ August 2020 LeetCoding Challenge Solution Java/Week 4/Solution28.java
@@ -1,1 +1,40 @@
-// Error 404
+/*
+\Implement Rand10() Using Rand7()
+
+Given the API rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10. You can only call the API rand7 and you shouldn't call any other API. Please don't use the system's Math.random().
+
+Notice that Each test case has one argument n, the number of times that your implemented function rand10 will be called while testing. 
+
+Follow up:
+
+    What is the expected value for the number of calls to rand7() function?
+    Could you minimize the number of calls to rand7()?
+
+ 
+
+Example 1:
+
+Input: n = 1
+Output: [2]
+
+Example 2:
+
+Input: n = 2
+Output: [2,8]
+*/
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        int row, col, idx;
+        do {
+            row = rand7();
+            col = rand7();
+            idx = col + (row - 1) * 7;
+        } while (idx > 40);
+        return 1 + (idx - 1) % 10;
+    }
+}
