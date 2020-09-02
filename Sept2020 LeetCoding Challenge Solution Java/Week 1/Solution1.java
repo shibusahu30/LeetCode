d71f1fc65b5392bb89220140cf1dbd08b47f3082
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-09-02 07:55:09 +0530

    DFS, BT, Day 1 solution added.

--- Sept2020 LeetCoding Challenge Solution Java/Week 1/Solution1.java
+++ Sept2020 LeetCoding Challenge Solution Java/Week 1/Solution1.java
@@ -1,1 +1,67 @@
-// Error 404
+/*
+Largest Time for Given Digits
+
+Given an array of 4 digits, return the largest 24 hour time that can be made.
+
+The smallest 24 hour time is 00:00, and the largest is 23:59.  Starting from 00:00, a time is larger if more time has elapsed since midnight.
+
+Return the answer as a string of length 5.  If no valid time can be made, return an empty string.
+
+ 
+
+Example 1:
+
+Input: [1,2,3,4]
+Output: "23:41"
+
+Example 2:
+
+Input: [5,5,5,5]
+Output: ""
+
+ 
+
+Note:
+
+    A.length == 4
+    0 <= A[i] <= 9
+*/
+class Solution {
+    private int max_time = -1;
+
+    public String largestTimeFromDigits(int[] A) {
+        this.max_time = -1;
+        permutate(A, 0);
+        if (this.max_time == -1)
+            return "";
+        else
+            return String.format("%02d:%02d", max_time / 60, max_time % 60);
+    }
+
+    protected void permutate(int[] array, int start) {
+        if (start == array.length) {
+            this.build_time(array);
+            return;
+        }
+        for (int i = start; i < array.length; ++i) {
+            this.swap(array, i, start);
+            this.permutate(array, start + 1);
+            this.swap(array, i, start);
+        }
+    }
+
+    protected void build_time(int[] perm) {
+        int hour = perm[0] * 10 + perm[1];
+        int minute = perm[2] * 10 + perm[3];
+        if (hour < 24 && minute < 60)
+            this.max_time = Math.max(this.max_time, hour * 60 + minute);
+    }
+
+    protected void swap(int[] array, int i, int j) {
+        if (i != j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
