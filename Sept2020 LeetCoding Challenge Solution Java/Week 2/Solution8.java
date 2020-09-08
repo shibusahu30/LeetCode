060dc105a75de2259d8048d48380e5a98e06a18d
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-09-08 13:08:24 +0530

    Day 8 solution added.

--- Sept2020 LeetCoding Challenge Solution Java/Week 2/Solution8.java
+++ Sept2020 LeetCoding Challenge Solution Java/Week 2/Solution8.java
@@ -1,1 +1,64 @@
-// Error 404
+/*
+Sum of Root To Leaf Binary Numbers
+
+Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+ 
+
+Example 1:
+
+          1
+         / \
+        0   1
+       / \ / \
+       0 1 0  1
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+ 
+
+Note:
+
+    The number of nodes in the tree is between 1 and 1000.
+    node.val is 0 or 1.
+    The answer will not exceed 2^31 - 1.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum;
+    public int sumRootToLeaf(TreeNode root) {
+        sum = 0;
+        dfs(root, 0);
+        return sum;
+    }
+    void dfs(TreeNode r, int l) {
+        if(r == null) return;
+        
+        if(r.left == null && r.right == null) {
+            sum += ((l<<1)+r.val);
+            return;
+        }
+        dfs(r.left, (l<<1)+r.val);
+        dfs(r.right, (l<<1)+r.val);
+        return;
+    }
+}
