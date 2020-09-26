4a37042366193409dca22e02b8071365f32125a4
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-09-26 16:23:44 +0530

    Day 25 solution added.

--- Sept2020 LeetCoding Challenge Solution Java/Week 4/Solution25.java
+++ Sept2020 LeetCoding Challenge Solution Java/Week 4/Solution25.java
@@ -1,1 +1,53 @@
-// Error 404
+/*
+Largest Number
+
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+Example 1:
+
+Input: [10,2]
+Output: "210"
+
+Example 2:
+
+Input: [3,30,34,5,9]
+Output: "9534330"
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+
+*/
+class Solution {
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+           return b.compareTo(a);
+        }
+    }
+
+    public String largestNumber(int[] nums) {
+        // Get input integers as strings.
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+
+        // Sort strings according to custom comparator.
+        Arrays.sort(asStrs, new LargerNumberComparator());
+
+        // If, after being sorted, the largest number is `0`, the entire number
+        // is zero.
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+
+        // Build largest number from sorted array.
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
+    }
+}
