321f0cc31be427d36a7c4d8ba39310d9e081f8cd
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-04 14:39:49 +0530

    Day 4 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 1/Solution4.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 1/Solution4.java
@@ -1,1 +1,62 @@
-// Error 404
+/*
+Remove Covered Intervals
+
+Given a list of intervals, remove all intervals that are covered by another interval in the list.
+
+Interval [a,b) is covered by interval [c,d) if and only if c <= a and b <= d.
+
+After doing so, return the number of remaining intervals.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+
+Example 2:
+
+Input: intervals = [[1,4],[2,3]]
+Output: 1
+
+Example 3:
+
+Input: intervals = [[0,10],[5,12]]
+Output: 2
+
+Example 4:
+
+Input: intervals = [[3,10],[4,10],[5,11]]
+Output: 2
+
+Example 5:
+
+Input: intervals = [[1,2],[1,4],[3,4]]
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= intervals.length <= 1000
+    intervals[i].length == 2
+    0 <= intervals[i][0] < intervals[i][1] <= 10^5
+    All the intervals are unique.
+*/
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        if(intervals.length == 0 || intervals.length == 1) return intervals.length;
+        int ans = 0;
+        Arrays.sort(intervals, (x, y)->x[0]==y[0]?-Integer.compare(x[1], y[1]):Integer.compare(x[0], y[0]));
+        
+        int r = 0;
+        for(int[] it: intervals)  {
+            if(it[1] > r) {
+                ++ans;
+                r = it[1];
+            }
+        }
+        return ans;
+    }
+}
