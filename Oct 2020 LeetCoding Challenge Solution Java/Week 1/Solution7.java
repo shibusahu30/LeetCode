ee6ff4621df2e45a6d72b3a7cac8c0839684d040
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-08 07:54:25 +0530

    Day 7 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 1/Solution7.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 1/Solution7.java
@@ -1,1 +1,51 @@
-// Error 404
+/*
+Rotate List
+
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null) return head;
+        int size = 1;
+        ListNode t = head;
+        while(t.next != null) {
+            ++size;
+            t = t.next;
+        }
+        ListNode slow = head;
+        for(int i=size-k%size;i>1;i--) slow = slow.next;
+        
+        t.next = head;
+        head = slow.next;
+        slow.next = null;
+        return head;
+    }
+}
