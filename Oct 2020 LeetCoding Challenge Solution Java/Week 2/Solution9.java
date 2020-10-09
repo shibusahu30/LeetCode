fc1cce41580f1c993e640cdfafd077cd10a5fd35
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-09 20:39:16 +0530

    Day 9 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 2/Solution9.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 2/Solution9.java
@@ -1,1 +1,71 @@
-// Error 404
+/*
+Serialize and Deserialize BST
+
+Serialization is converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You need to ensure that a binary search tree can be serialized to a string, and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+ 
+
+Example 1:
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+
+Example 2:
+
+Input: root = []
+Output: []
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serializable(root, sb);
+        return sb.toString();
+    }
+    
+    public void serializable(TreeNode r, StringBuilder sb) {
+        if(r == null) return;
+        sb.append(r.val).append(",");
+        serializable(r.left, sb);
+        serializable(r.right, sb);
+    }
+    
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data.isEmpty()) return null;
+        Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
+        return deserialize(q, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    public TreeNode deserialize(Queue<String> q, int min, int max) {
+        if(q.isEmpty()) return null;
+        String s = q.peek();
+        int x = Integer.parseInt(s);
+        if(x < min || x > max) return null;
+        q.poll();
+        TreeNode t = new TreeNode(x);
+        t.left = deserialize(q, min, x);
+        t.right = deserialize(q, x, max);
+        return t;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// String tree = ser.serialize(root);
+// TreeNode ans = deser.deserialize(tree);
+// return ans;
