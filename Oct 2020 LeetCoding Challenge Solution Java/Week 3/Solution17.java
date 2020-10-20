b0b28ae518d499c6eb2be17d2bd379e1b7905d8a
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-20 10:07:05 +0530

    Day 17 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution17.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution17.java
@@ -1,47 +1,38 @@
 /*
-Rotate Array
+Repeated DNA Sequences
 
-Given an array, rotate the array to the right by k steps, where k is non-negative.
+All DNA is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T', for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
 
-Follow up:
-
-    Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
-    Could you do it in-place with O(1) extra space?
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
 
  
 
 Example 1:
 
-Input: nums = [1,2,3,4,5,6,7], k = 3
-Output: [5,6,7,1,2,3,4]
-Explanation:
-rotate 1 steps to the right: [7,1,2,3,4,5,6]
-rotate 2 steps to the right: [6,7,1,2,3,4,5]
-rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
 
 Example 2:
 
-Input: nums = [-1,-100,3,99], k = 2
-Output: [3,99,-1,-100]
-Explanation: 
-rotate 1 steps to the right: [99,-1,-100,3]
-rotate 2 steps to the right: [3,99,-1,-100]
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+
+ 
+
+Constraints:
+
+    0 <= s.length <= 105
+    s[i] is 'A', 'C', 'G', or 'T'.
 */
 class Solution {
-  public void rotate(int[] nums, int k) {
-    k = k % nums.length;
-    int count = 0;
-    for (int start = 0; count < nums.length; start++) {
-      int current = start;
-      int prev = nums[start];
-      do {
-        int next = (current + k) % nums.length;
-        int temp = nums[next];
-        nums[next] = prev;
-        prev = temp;
-        current = next;
-        count++;
-      } while (start != current);
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> first = new HashSet<>();
+        HashSet<String> second = new HashSet<>();
+        for(int i = 0; i < s.length()-9; ++i) {
+            String ss = s.substring(i, i+10);
+            if(first.contains(ss)) second.add(ss);
+            first.add(ss);
+        }
+        return new ArrayList(second);
     }
-  }
 }
