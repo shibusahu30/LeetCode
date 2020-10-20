b34c01dfe4daa46dbf0dcf6ba02bfbd6e21a0dc6
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-20 10:08:03 +0530

    Day 18 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution18.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution18.java
@@ -1,23 +1,88 @@
 /*
-Search a 2D Matrix
+Best Time to Buy and Sell Stock IV
 
-Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
-    Integers in each row are sorted from left to right.
-    The first integer of each row is greater than the last integer of the previous row.
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+
+Notice that you may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: k = 2, prices = [2,4,1]
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+
+Example 2:
+
+Input: k = 2, prices = [3,2,6,5,0,3]
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+
+ 
+
+Constraints:
+
+    0 <= k <= 109
+    0 <= prices.length <= 104
+    0 <= prices[i] <= 1000
+
+
+
 */
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length < 1 || matrix[0].length <1) {
-            return false;
+public class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+
+        // solve special cases
+        if (n <= 0 || k <= 0) {
+            return 0;
         }
-        int r = 0;
-        int c = matrix[0].length-1;
-        while(r < matrix.length && c >= 0) {
-            if(matrix[r][c] == target) return true;
-            else if(matrix[r][c] < target) ++r;
-            else --c;
+
+        if (2 * k > n) {
+            int res = 0;
+            for (int i = 1; i < n; i++) {
+                res += Math.max(0, prices[i] - prices[i - 1]);
+            }
+            return res;
         }
-        return false;
+
+        // dp[i][used_k][ishold] = balance
+        // ishold: 0 nothold, 1 hold
+        int[][][] dp = new int[n][k + 1][2];
+
+        // initialize the array with -inf
+        // we use -1e9 here to prevent overflow
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                dp[i][j][0] = -1000000000;
+                dp[i][j][1] = -1000000000;
+            }
+        }
+
+        // set starting value
+        dp[0][0][0] = 0;
+        dp[0][1][1] = -prices[0];
+
+        // fill the array
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= k; j++) {
+                // transition equation
+                dp[i][j][0] = Math.max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                // you can't hold stock without any transaction
+                if (j > 0) {
+                    dp[i][j][1] = Math.max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int j = 0; j <= k; j++) {
+            res = Math.max(res, dp[n - 1][j][0]);
+        }
+
+        return res;
     }
 }
