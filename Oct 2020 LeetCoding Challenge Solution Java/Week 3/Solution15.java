bfcc9dd679d097d08965864b72cec5d301b3195f
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-10-20 10:04:36 +0530

    Day 15 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution15.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 3/Solution15.java
@@ -1,81 +1,40 @@
 /*
- Sort List
+Rotate Array
 
-Given the head of a linked list, return the list after sorting it in ascending order.
+Given an array, rotate the array to the right by k steps, where k is non-negative.
 
-Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+Follow up:
+
+    Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+    Could you do it in-place with O(1) extra space?
 
  
 
 Example 1:
 
-Input: head = [4,2,1,3]
-Output: [1,2,3,4]
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
-Example 2:
-
-Input: head = [-1,5,3,4,0]
-Output: [-1,0,3,4,5]
-
-Example 3:
-
-Input: head = []
-Output: []
-
- 
-
-Constraints:
-
-    The number of nodes in the list is in the range [0, 5 * 104].
-    -105 <= Node.val <= 105
 */
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class Solution {
-    public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode mid = getMid(head);
-        ListNode left = sortList(head);
-        ListNode right = sortList(mid);
-        return merge(left, right);
+  public void rotate(int[] nums, int k) {
+    k = k % nums.length;
+    int count = 0;
+    for (int start = 0; count < nums.length; start++) {
+      int current = start;
+      int prev = nums[start];
+      do {
+        int next = (current + k) % nums.length;
+        int temp = nums[next];
+        nums[next] = prev;
+        prev = temp;
+        current = next;
+        count++;
+      } while (start != current);
     }
-
-    ListNode merge(ListNode list1, ListNode list2) {
-        ListNode dummyHead = new ListNode();
-        ListNode tail = dummyHead;
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                tail.next = list1;
-                list1 = list1.next;
-                tail = tail.next;
-            } else {
-                tail.next = list2;
-                list2 = list2.next;
-                tail = tail.next;
-            }
-        }
-        tail.next = (list1 != null) ? list1 : list2;
-        return dummyHead.next;
-    }
-
-    ListNode getMid(ListNode head) {
-        ListNode midPrev = null;
-        while (head != null && head.next != null) {
-            midPrev = (midPrev == null) ? head : midPrev.next;
-            head = head.next.next;
-        }
-        ListNode mid = midPrev.next;
-        midPrev.next = null;
-        return mid;
-    }
+  }
 }
