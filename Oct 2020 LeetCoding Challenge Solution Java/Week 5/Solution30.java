d39d0832b2af8eb17b1228e9a8e07bd065957afb
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-11-02 07:15:30 +0530

    DSA, 5, Day 30 solution added.

--- Oct 2020 LeetCoding Challenge Solution Java/Week 5/Solution30.java
+++ Oct 2020 LeetCoding Challenge Solution Java/Week 5/Solution30.java
@@ -1,1 +1,72 @@
-// Error 404
+/*
+Recover Binary Search Tree
+
+You are given the root of a binary search tree (BST), where exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+Follow up: A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
+
+ 
+
+Example 1:
+
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2:
+
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [2, 1000].
+    -231 <= Node.val <= 231 - 1
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
