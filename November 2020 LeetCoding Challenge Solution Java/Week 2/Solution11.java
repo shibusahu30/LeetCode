62b242780c8c2c62d1502b04e4788ef1da247a7e
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-11-19 07:41:51 +0530

    Day 11 solution added.

--- November 2020 LeetCoding Challenge Solution Java/Week 2/Solution11.java
+++ November 2020 LeetCoding Challenge Solution Java/Week 2/Solution11.java
@@ -1,1 +1,44 @@
-// Error 404
+/*
+Valid Square
+
+Given the coordinates of four points in 2D space p1, p2, p3 and p4, return true if the four points construct a square.
+
+The coordinate of a point pi is represented as [xi, yi]. The input is not given in any order.
+
+A valid square has four equal sides with positive length and four equal angles (90-degree angles).
+
+ 
+
+Example 1:
+
+Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,1]
+Output: true
+
+Example 2:
+
+Input: p1 = [0,0], p2 = [1,1], p3 = [1,0], p4 = [0,12]
+Output: false
+
+Example 3:
+
+Input: p1 = [1,0], p2 = [-1,0], p3 = [0,1], p4 = [0,-1]
+Output: true
+
+ 
+
+Constraints:
+
+    p1.length == p2.length == p3.length == p4.length == 2
+    -104 <= xi, yi <= 104
+*/
+public class Solution {
+    public double dist(int[] p1, int[] p2) {
+        return (p2[1] - p1[1]) * (p2[1] - p1[1]) + (p2[0] - p1[0]) * (p2[0] - p1[0]);
+    }
+    public boolean check(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return dist(p1,p2) > 0 && dist(p1, p2) == dist(p2, p3) && dist(p2, p3) == dist(p3, p4) && dist(p3, p4) == dist(p4, p1) && dist(p1, p3) == dist(p2, p4);
+    }
+    public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        return check(p1, p2, p3, p4) || check(p1, p3, p2, p4) || check(p1, p2, p4, p3);
+    }
+}
