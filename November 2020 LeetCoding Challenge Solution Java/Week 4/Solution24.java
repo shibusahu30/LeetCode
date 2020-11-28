f1ae1605585cfaf397cfed987ac5f0bf61dd883c
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-11-28 16:18:24 +0530

    Day 24 solution added.

--- November 2020 LeetCoding Challenge Solution Java/Week 4/Solution24.java
+++ November 2020 LeetCoding Challenge Solution Java/Week 4/Solution24.java
@@ -1,1 +1,53 @@
-// Error 404
+/*
+Basic Calculator II
+
+Given a string s which represents an expression, evaluate this expression and return its value. 
+
+The integer division should truncate toward zero.
+
+ 
+
+Example 1:
+
+Input: s = "3+2*2"
+Output: 7
+
+Example 2:
+
+Input: s = " 3/2 "
+Output: 1
+
+Example 3:
+
+Input: s = " 3+5 / 2 "
+Output: 5
+*/
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitespace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+        
+    }
+}
