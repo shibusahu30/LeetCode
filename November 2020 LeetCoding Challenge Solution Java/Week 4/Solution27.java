a6023e6dcbe3308521ecf36210997fb261343deb
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-11-28 16:21:04 +0530

    DP, Day 27 solution added.

--- November 2020 LeetCoding Challenge Solution Java/Week 4/Solution27.java
+++ November 2020 LeetCoding Challenge Solution Java/Week 4/Solution27.java
@@ -1,1 +1,48 @@
-// Error 404
+/*
+Partition Equal Subset Sum
+
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 200
+    1 <= nums[i] <= 100
+*/
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if (nums.length == 0)
+            return false;
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd,it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        int subSetSum = totalSum / 2;
+        boolean dp[] = new boolean[subSetSum + 1];
+        dp[0] = true;
+        for (int curr : nums) {
+            for (int j = subSetSum; j >= curr; j--) {
+                dp[j] |= dp[j - curr];
+            }
+        }
+        return dp[subSetSum];
+    }
+}
