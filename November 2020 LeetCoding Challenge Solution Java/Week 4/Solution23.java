d171f8aa1dec17d0220348ee43d0e43b820b664e
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-11-28 16:17:16 +0530

    DP, Tree, 5, Day 23 solution added.

--- November 2020 LeetCoding Challenge Solution Java/Week 4/Solution23.java
+++ November 2020 LeetCoding Challenge Solution Java/Week 4/Solution23.java
@@ -1,1 +1,104 @@
-// Error 404
+/*
+House Robber III
+
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Determine the maximum amount of money the thief can rob tonight without alerting the police.
+
+Example 1:
+
+Input: [3,2,3,null,3,null,1]
+
+     3
+    / \
+   2   3
+    \   \ 
+     3   1
+
+Output: 7 
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+
+Example 2:
+
+Input: [3,4,5,1,3,null,1]
+
+     3
+    / \
+   4   5
+  / \   \ 
+ 1   3   1
+
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // reform tree into array-based tree
+        ArrayList<Integer> tree = new ArrayList<>();
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        graph.put(-1, new ArrayList<>());
+        int index = -1;
+        // we use two Queue to store node and index
+        Queue<TreeNode> q_node = new LinkedList<>();
+        q_node.add(root);
+        Queue<Integer> q_index = new LinkedList<>();
+        q_index.add(index);
+
+        while (q_node.size() > 0) {
+            TreeNode node = q_node.poll();
+            int parentIndex = q_index.poll();
+            if (node != null) {
+                index++;
+                tree.add(node.val);
+                graph.put(index, new ArrayList<>());
+                graph.get(parentIndex).add(index);
+                // push new node into Queue
+                q_node.add(node.left);
+                q_index.add(index);
+                q_node.add(node.right);
+                q_index.add(index);
+            }
+        }
+
+        // represent the maximum start by node i with robbing i
+        int[] dpRob = new int[index + 1];
+
+        // represent the maximum start by node i without robbing i
+        int[] dpNotRob = new int[index + 1];
+
+        for (int i = index; i >= 0; i--) {
+            ArrayList<Integer> children = graph.get(i);
+            if (children == null || children.size() == 0) {
+                // if is leaf
+                dpRob[i] = tree.get(i);
+                dpNotRob[i] = 0;
+            } else {
+                dpRob[i] = tree.get(i);
+                for (int child : children) {
+                    dpRob[i] += dpNotRob[child];
+                    dpNotRob[i] += Math.max(dpRob[child], dpNotRob[child]);
+                }
+            }
+        }
+
+        return Math.max(dpRob[0], dpNotRob[0]);
+    }
+}
