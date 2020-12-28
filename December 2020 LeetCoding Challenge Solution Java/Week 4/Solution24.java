b9abe058efb327e0f3d69663befb6fe31048fac5
Shibashis Sahu <44725484+shibusahu30@users.noreply.github.com>
2020-12-28 19:47:34 +0530

    Day 24 solution added.

--- December 2020 LeetCoding Challenge Solution Java/Week 4/Solution24.java
+++ December 2020 LeetCoding Challenge Solution Java/Week 4/Solution24.java
@@ -1,1 +1,54 @@
-// Error 404
+/*
+Swap Nodes in Pairs
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes. Only nodes itself may be changed.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4]
+Output: [2,1,4,3]
+
+Example 2:
+
+Input: head = []
+Output: []
+
+Example 3:
+
+Input: head = [1]
+Output: [1]
+
+ 
+
+Constraints:
+
+    The number of nodes in the list is in the range [0, 100].
+    0 <= Node.val <= 100
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode t = head;
+        while(t != null && t.next != null) {
+            int x = t.val;
+            t.val = t.next.val;
+            t.next.val = x;
+            t = t.next.next;
+        }
+        return head;
+    }
+}
